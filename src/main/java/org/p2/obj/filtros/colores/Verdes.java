dc73bc34d516fb22ce78358a07c9611b1b832fa6
ecf17 <74380A@nur.edu.bo>
2023-04-24 23:30:02 -0400

    eliminado codigo anterior

--- src/main/java/org/p2/obj/filtros/colores/Verdes.java
+++ src/main/java/org/p2/obj/filtros/colores/Verdes.java
@@ -13,16 +13,15 @@ public class Verdes extends ComandoFiltro {
     public void ejecutar() {
         int ancho = imagenBase.getAncho();
         int alto = imagenBase.getAlto();
-        int[][] pixelesOriginales = imagenBase.getPixelesOriginales();
-        int[][] pixelesNuevos= imagenBase.getPixeles();
+        int[][] pixeles = imagenBase.getPixeles();
         for (int i = 0; i < ancho; i++) {
             for (int j = 0; j < alto; j++) {
                 // Obtiene el valor de verde del píxel
-                int g = (pixelesOriginales[i][j] >> 8) & 0x000000FF;
+                int g = (pixeles[i][j] >> 8) & 0x000000FF;
                 // Creamos un nuevo valor entero que representa el color verde del píxel,
                 // manteniendo los otros componentes de color en cero
                 int verde = (g << 8) + 0xFF000000;
-                pixelesNuevos[i][j] = verde;
+                pixeles[i][j] = verde;
             }
         }
         imagenBase.cambiosImagen();
